0ea243f1315b41f3da84ed0a4317fce3ca94f665
Hamed Bahram <hamed.bahram@gmail.com>
2024-05-03 08:53:33 -0400

    Move todos function to shared database workspace

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,7 +3,7 @@ import { Card } from "@repo/ui/card";
 import { Code } from "@repo/ui/code";
 import styles from "./page.module.css";
 import { Button } from "@repo/ui/button";
-import { getTodos } from "../lib/todos";
+import { getTodos } from "database";
 
 function Gradient({
   conic,
